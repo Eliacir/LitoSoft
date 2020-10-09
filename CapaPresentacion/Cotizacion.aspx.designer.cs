2691f2f777cda3aedaffc61f5b6bdb3631e6ccb1
eliacir herazo meza <eaprendiz@outlook.es>
2020-10-09 02:28:43 -0500

    .

--- CapaPresentacion/Cotizacion.aspx.designer.cs
+++ CapaPresentacion/Cotizacion.aspx.designer.cs
@@ -229,15 +229,7 @@ namespace CapaPresentacion
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtRespaldo;
-        
-        /// <summary>
-        /// Control txtRespaldo.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddMismaplancha;
+
         
         /// <summary>
         /// Control txtValorTotalImpresiones.
